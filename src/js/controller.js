3d2c26d1724769dd77f5238407b3793e3cc5f9f5
boryslatun <latunboris87@gmail.com>
2022-10-29 18:26:34 +0300

    Added new features

--- src/js/controller.js
+++ src/js/controller.js
@@ -112,9 +112,9 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature-function(){
+const newFeature = function () {
   console.log('Welcome ti the new application!');
-}
+};
 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmark);
@@ -124,6 +124,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
- newFeature()
+  newFeature();
 };
 init();
