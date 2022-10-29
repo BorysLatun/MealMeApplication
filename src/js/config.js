999059044b04d9acc37bc5b44dd8e12891fe041b
boryslatun <latunboris87@gmail.com>
2022-10-29 17:52:15 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3034a5ad-3f39-4964-98f1-a39037ff94b5';
+export const MODAL_CLOSE_SEC = 2.5;
